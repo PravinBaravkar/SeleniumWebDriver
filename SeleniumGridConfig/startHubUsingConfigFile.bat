1b298c2106a94640c554b0116ef8620daed34b4d
PravinBaravkar <baravkar.pravin1@gmail.com>
2021-02-01 17:30:32 +0530

    selenium grid config

---
+++ SeleniumGridConfig/startHubUsingConfigFile.bat
@@ -0,0 +1,2 @@
+java -jar selenium-server-standalone-3.10.0.jar -role hub -hubConfig HubConfig.json -debug
+pause
---
+++ SeleniumGridConfig/startNode.bat
@@ -0,0 +1,2 @@
+java -Dwebdriver.chrome.driver="chromedriver.exe --whitelisted-all-ips=''" -Dwebdriver.gecko.driver=geckodriver.exe -Dwebdriver.ie.driver=IEDriverServer.exe -jar selenium-server-standalone-3.10.0.jar -role node -nodeConfig nodeConfig.json
+pause
